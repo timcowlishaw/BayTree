2ca53688ad6c97ef136c21e3724f6698a61f4448
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-01-11 17:25:38 +0000

    add a CDF function to RandomVariable

--- src/main/scala/uk/co/timcowlishaw/BayTree/RandomVariable.scala
+++ src/main/scala/uk/co/timcowlishaw/BayTree/RandomVariable.scala
@@ -1,36 +1,47 @@
 package uk.co.timcowlishaw.BayTree;
-import scala.collection.immutable.HashMap
+import scala.collection.immutable.TreeMap
+import scala.collection.immutable.SortedSet
+import scala.collection.immutable.Set
+
 
 object RandomVariable {
-  def uniform[T](set : Set[T]) : RandomVariable[T] = new RandomVariable(set map {(_,1.0)});
-  // Some more discrete distributions!
-  //def binomial;
-  //def zeta
-  //def bernoulli(1, 2);
+
+  def uniform[T](set : Set[T])(implicit ev : Ordering[T]) : RandomVariable[T] = {
+    new RandomVariable(set map { (_, 1.0) });
+  }  
+    // Some more discrete distributions!
+    //def binomial;
+    //def zeta
+    //def bernoulli(1, 2);
 }
 
-class RandomVariable[T](private val members: Set[Tuple2[T, Double]]) {
+class RandomVariable[T] (private val members: Set[(T, Double)]) (implicit ev : Ordering[T]) {
+
   private val total = (0.0 /: members) { (total, pair) => 
     val (_, prob) = pair
     total + prob
   }
-  private val dist = (new HashMap[T, Double] /: members) { (hash, pair)  => 
+
+  private val dist = (new TreeMap[T, Double] /: members) { (map, pair) =>
     val (item, prob) = pair
-    hash + ((item, prob / total))
+    map + ((item, prob / total))
   }
 
   def p(v:T) : Double = dist.getOrElse(v, 0)
 
-  //def c(v:T) : Double // REQUIRES ORDERING OVER MEMBER VALUES - Iterable rather than Set for EG.
+  def c(v:T) : Double = (0.0 /: dist.filterKeys { ev.lteq(_, v) }) { (sum, pair) =>
+    val (_, prob) = pair
+    sum + prob
+  }
 
   def p(f : T => Boolean) : Double =  {
     val chosen = elements filter f;
     (0.0 /: chosen) { (total, element) => total + p(element) }
   }
 
-  def elements : Set[T] = dist.keySet;
-
-  def and[U](other : RandomVariable[U]) : RandomVariable[(T,U)] = {
+  def elements : SortedSet[T] = dist.keySet;
+  
+  def and[U](other : RandomVariable[U])(implicit ev : Ordering[(T,U)]) : RandomVariable[(T,U)] = {
     val pairs = for {
       i <- elements
       j <- other.elements
--- src/test/scala/uk/co/timcowlishaw/BayTree/RandomVariable.scala
+++ src/test/scala/uk/co/timcowlishaw/BayTree/RandomVariable.scala
@@ -6,7 +6,7 @@ class RandomVariableSpec extends Specification {
 
   "Creating a uniformly distributed random variable" should {
     "return a random variable with the same probability for each element in the set" in {
-      val values = Set("One", "Two", "Three"); 
+      val values = Set(1, 2, 3); 
       val rv = RandomVariable.uniform(values);
       values.map { rv.p(_) }.size mustEqual 1
     }
@@ -15,11 +15,11 @@ class RandomVariableSpec extends Specification {
   "Getting the probability for an element" should {
     
     "return the value given for the item divided by the total value of every item in the set, if the item is in the random variable's set" in new randomVariable {
-      rv.p("One") mustEqual 1.0/3
+      rv.p(1) mustEqual 1.0/3
     }
 
     "return 0 if the element is not in the random variable's set" in new randomVariable {
-      rv.p("Four") mustEqual 0
+      rv.p(4) mustEqual 0
     }
 
     "return values that add up to 1 for all elements in the set" in new randomVariable {
@@ -30,18 +30,24 @@ class RandomVariableSpec extends Specification {
 
   "Getting the probability for all elements matching a predicate" should {
     "return the sum of the probabilities of each item" in new randomVariable {
-      rv.p { s => Seq("One", "Two") contains s } mustEqual 2.0/3
+      rv.p { s => Seq(1, 2) contains s } mustEqual 2.0/3
+    }
+  }
+
+  "Getting the cumulative probability for an element" should {
+    "return the sum of all the probabilities of each item less than or equal to this one" in new randomVariable {
+      rv.c(2) mustEqual 2.0/3
     }
   }
 
   "Getting the elements" should {
     "return a set of all elements in the random variable" in new randomVariable {
-      rv.elements mustEqual Set("One", "Two", "Three")
+      rv.elements mustEqual Set(1, 2, 3)
     }
   }
 
   "The joint distribution of two random variables" should {
-    "have an elements set containing the product of both random variables' elements" in new jointRandomVariable {
+    "have an elements set containing the product of both random variables' elements" in new jointRandomVariable{
       jointRv.elements mustEqual elements
     }
 
@@ -58,16 +64,16 @@ class RandomVariableSpec extends Specification {
   }
 
   trait randomVariable extends Scope {
-    val rv : RandomVariable[String] = new RandomVariable(Set(("One", 1), ("Two", 1), ("Three", 1)))
+    val rv : RandomVariable[Int] = new RandomVariable(Set((1, 1), (2, 1), (3, 1)))
   }
 
   trait jointRandomVariable extends Scope {
-    val rv1 : RandomVariable[String] = RandomVariable.uniform(Set("One", "Two", "Three"));
+    val rv1 : RandomVariable[Int] = RandomVariable.uniform(Set(1, 2, 3));
     val rv2 : RandomVariable[String] = RandomVariable.uniform(Set("Ay", "Bee", "Cee", "Dee"));
 
     val jointRv = rv1 and rv2;
     
-    val elements : Set[Tuple2[String, String]] = for {
+    val elements : Set[Tuple2[Int, String]] = for {
       i <- rv1.elements
       j <- rv2.elements
     } yield (i,j);
